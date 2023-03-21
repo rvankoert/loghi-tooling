db0ad6f85c8bc55779367d5866219cf6f90b674d
Martijn Maas <martijn.maas@di.huc.knaw.nl>
2023-03-21 11:15:04 +0100

    Update PrintSpace

--- layoutds/src/main/java/nl/knaw/huc/di/images/layoutds/models/Page/PrintSpace.java
+++ layoutds/src/main/java/nl/knaw/huc/di/images/layoutds/models/Page/PrintSpace.java
@@ -1,4 +1,20 @@
 package nl.knaw.huc.di.images.layoutds.models.Page;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+
+// FIXME should not extend from Region
 public class PrintSpace extends Region {
+
+    @JacksonXmlProperty(localName = "Coords", namespace = "http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15")
+    private Coords coords;
+
+    @Override
+    public Coords getCoords() {
+        return coords;
+    }
+
+    @Override
+    public void setCoords(Coords coords) {
+        this.coords = coords;
+    }
 }
