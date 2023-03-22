68434721aa89f654e35cd6d52ec8d17f4f69b0fd
Martijn Maas <martijn.maas@di.huc.knaw.nl>
2023-03-22 10:47:31 +0100

    Update ReadingOrder

--- layoutds/src/main/java/nl/knaw/huc/di/images/layoutds/models/Page/ReadingOrder.java
+++ layoutds/src/main/java/nl/knaw/huc/di/images/layoutds/models/Page/ReadingOrder.java
@@ -6,11 +6,33 @@ public class ReadingOrder {
     @JacksonXmlProperty(localName = "OrderedGroup", namespace = "http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15")
     private OrderedGroup orderedGroup;
 
+    @JacksonXmlProperty(localName = "UnorderedGroup", namespace = "http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15")
+    private UnorderedGroup unorderedGroup;
+
+    @JacksonXmlProperty(isAttribute = true, localName = "conf")
+    private Double confidence;
+
     public OrderedGroup getOrderedGroup() {
         return orderedGroup;
     }
 
     public void setOrderedGroup(OrderedGroup orderedGroup) {
         this.orderedGroup = orderedGroup;
+    }
+
+    public UnorderedGroup getUnorderedGroup() {
+        return unorderedGroup;
+    }
+
+    public void setUnorderedGroup(UnorderedGroup unorderedGroup) {
+        this.unorderedGroup = unorderedGroup;
+    }
+
+    public Double getConfidence() {
+        return confidence;
+    }
+
+    public void setConfidence(Double confidence) {
+        this.confidence = confidence;
     }
 }
---
+++ layoutds/src/main/java/nl/knaw/huc/di/images/layoutds/models/Page/UnorderedGroup.java
@@ -0,0 +1,140 @@
+package nl.knaw.huc.di.images.layoutds.models.Page;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+
+import java.util.List;
+
+public class UnorderedGroup {
+    @JacksonXmlProperty(localName = "UserDefined", namespace = "http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15")
+    private UserDefined userDefined;
+
+    @JacksonXmlProperty(localName = "Labels", namespace = "http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15")
+    private List<Labels> labelsList;
+
+    @JacksonXmlProperty(localName = "RegionRef", namespace = "http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15")
+    private List<RegionRef> regionRefs;
+
+    @JacksonXmlProperty(localName = "OrderedGroup", namespace = "http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15")
+    private List<OrderedGroup> orderedGroups;
+
+    @JacksonXmlProperty(localName = "UnorderedGroup", namespace = "http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15")
+    private List<UnorderedGroup> unorderedGroups;
+
+    @JacksonXmlProperty(isAttribute = true)
+    private String id;
+
+    @JacksonXmlProperty(isAttribute = true)
+    private String regionRef;
+
+    @JacksonXmlProperty(isAttribute = true)
+    private String caption;
+
+    // TODO make enum with values: paragraph, list, list-item, figure, article, div, other
+    @JacksonXmlProperty(isAttribute = true)
+    private String type;
+
+    @JacksonXmlProperty(isAttribute = true)
+    private Boolean continuation;
+
+    @JacksonXmlProperty(isAttribute = true)
+    private String custom;
+
+    @JacksonXmlProperty(isAttribute = true)
+    private String comments;
+
+    public UserDefined getUserDefined() {
+        return userDefined;
+    }
+
+    public void setUserDefined(UserDefined userDefined) {
+        this.userDefined = userDefined;
+    }
+
+    public List<Labels> getLabelsList() {
+        return labelsList;
+    }
+
+    public void setLabelsList(List<Labels> labelsList) {
+        this.labelsList = labelsList;
+    }
+
+    public List<RegionRef> getRegionRefs() {
+        return regionRefs;
+    }
+
+    public void setRegionRefs(List<RegionRef> regionRefs) {
+        this.regionRefs = regionRefs;
+    }
+
+    public List<OrderedGroup> getOrderedGroups() {
+        return orderedGroups;
+    }
+
+    public void setOrderedGroups(List<OrderedGroup> orderedGroups) {
+        this.orderedGroups = orderedGroups;
+    }
+
+    public List<UnorderedGroup> getUnorderedGroups() {
+        return unorderedGroups;
+    }
+
+    public void setUnorderedGroups(List<UnorderedGroup> unorderedGroups) {
+        this.unorderedGroups = unorderedGroups;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRegionRef() {
+        return regionRef;
+    }
+
+    public void setRegionRef(String regionRef) {
+        this.regionRef = regionRef;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Boolean getContinuation() {
+        return continuation;
+    }
+
+    public void setContinuation(Boolean continuation) {
+        this.continuation = continuation;
+    }
+
+    public String getCustom() {
+        return custom;
+    }
+
+    public void setCustom(String custom) {
+        this.custom = custom;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+}
