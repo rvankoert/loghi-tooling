83888fbf2b0500222a47fea3ccd134ba07f77492
Martijn Maas <martijn.maas@di.huc.knaw.nl>
2023-03-22 09:14:12 +0100

    Update AlternativeImage

--- layoutds/src/main/java/nl/knaw/huc/di/images/layoutds/models/Page/AlternativeImage.java
+++ layoutds/src/main/java/nl/knaw/huc/di/images/layoutds/models/Page/AlternativeImage.java
@@ -3,11 +3,35 @@ package nl.knaw.huc.di.images.layoutds.models.Page;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
 public class AlternativeImage {
-    @JacksonXmlProperty(localName = "filename")
+    @JacksonXmlProperty(isAttribute = true, localName = "filename")
     private String fileName;
-    @JacksonXmlProperty
+    @JacksonXmlProperty(isAttribute = true)
     private String comments;
-    @JacksonXmlProperty(localName = "conf")
-    // TODO check for range when deserializing
-    private float confidence;
+    @JacksonXmlProperty(isAttribute = true, localName = "conf")
+    // TODO find a to make sure the value is between 0 and 1
+    private Double confidence;
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public Double getConfidence() {
+        return confidence;
+    }
+
+    public void setConfidence(Double confidence) {
+        this.confidence = confidence;
+    }
 }
