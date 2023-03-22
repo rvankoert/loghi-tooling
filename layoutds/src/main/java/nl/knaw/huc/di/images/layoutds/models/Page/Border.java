771ecc4dee2b2794292fdf1fc13e23d59adf141c
Martijn Maas <martijn.maas@di.huc.knaw.nl>
2023-03-22 16:53:27 +0100

    Update Border

--- layoutds/src/main/java/nl/knaw/huc/di/images/layoutds/models/Page/Border.java
+++ layoutds/src/main/java/nl/knaw/huc/di/images/layoutds/models/Page/Border.java
@@ -1,4 +1,19 @@
 package nl.knaw.huc.di.images.layoutds.models.Page;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+
+// FIXME Border should not extend Region
 public class Border extends Region {
+    @JacksonXmlProperty(localName = "Coords", namespace = "http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15")
+    private Coords coords;
+
+    @Override
+    public Coords getCoords() {
+        return coords;
+    }
+
+    @Override
+    public void setCoords(Coords coords) {
+        this.coords = coords;
+    }
 }
