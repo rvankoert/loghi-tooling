04a923fc0445c8e5eb9d5f2d75567265fb782bb5
Martijn Maas <martijn.maas@di.huc.knaw.nl>
2023-03-23 09:03:31 +0100

    Update Baseline

--- layoutds/src/main/java/nl/knaw/huc/di/images/layoutds/models/Page/Baseline.java
+++ layoutds/src/main/java/nl/knaw/huc/di/images/layoutds/models/Page/Baseline.java
@@ -6,11 +6,22 @@ public class Baseline {
     @JacksonXmlProperty(isAttribute = true, localName = "points")
     private String points = "";
 
+    @JacksonXmlProperty(isAttribute = true, localName = "conf")
+    private Double confidence;
+
     public String getPoints() {
         return points;
     }
 
     public void setPoints(String points) {
         this.points = points;
+    }
+
+    public Double getConfidence() {
+        return confidence;
+    }
+
+    public void setConfidence(Double confidence) {
+        this.confidence = confidence;
     }
 }
