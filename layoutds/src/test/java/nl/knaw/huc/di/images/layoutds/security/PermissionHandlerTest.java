8377182b829c57ba293c8977f12fefc59ef38834
Martijn Maas <martijn.maas@di.huc.knaw.nl>
2023-06-06 14:26:20 +0200

    Let PermissionHandler dissallow disabled users

--- layoutds/src/test/java/nl/knaw/huc/di/images/layoutds/security/PermissionHandlerTest.java
+++ layoutds/src/test/java/nl/knaw/huc/di/images/layoutds/security/PermissionHandlerTest.java
@@ -170,6 +170,21 @@ public class PermissionHandlerTest {
     }
 
     @Test
+    public void isAllowedToCreateReturnsFalseWhenTheUserIsDisabled() {
+        final PimGroup pimGroup = new PimGroup();
+        pimGroupDAO.save(pimGroup);
+        final PimUser pimUser = userWithMembershipAndPrimaryGroup(pimGroup, Role.RESEARCHER);
+        pimUser.setDisabled(true);
+        pimUserDao.save(pimUser);
+
+        try (final Session session = SessionFactorySingleton.getSessionFactory().openSession()) {
+            final PimUser byUUID = pimUserDao.getByUUID(session, pimUser.getUuid());
+
+            assertThat(permissionHandler.isAllowedToCreate(session, byUUID), is(false));
+        }
+    }
+
+    @Test
     public void isAllowedToCreateReturnsTrueForAdminsWhenNotUsingGroups() {
         doNotUseGroups();
         final PimGroup pimGroup = new PimGroup();
@@ -229,6 +244,21 @@ public class PermissionHandlerTest {
     }
 
     @Test
+    public void isAllowedToCreateReturnsFalseIfTheUserIsDisabledAndNotUsingGroups() {
+        final PimGroup pimGroup = new PimGroup();
+        pimGroupDAO.save(pimGroup);
+        final PimUser pimUser = userWithRoles(List.of(Role.ADMIN));
+        pimUser.setDisabled(true);
+        pimUserDao.save(pimUser);
+
+        try (final Session session = SessionFactorySingleton.getSessionFactory().openSession()) {
+            final PimUser byUUID = pimUserDao.getByUUID(session, pimUser.getUuid());
+
+            assertThat(permissionHandler.isAllowedToCreate(session, byUUID), is(false));
+        }
+    }
+
+    @Test
     public void isAllowedToUpdateReturnsTrueForRoleWithUpdatePermission() {
         final PimGroup pimGroup = new PimGroup();
         pimGroupDAO.save(pimGroup);
@@ -257,12 +287,31 @@ public class PermissionHandlerTest {
         }
     }
 
+
     @Test
     public void isAllowedToUpdateReturnsFalseForRoleWithoutUpdatePermission() {
         final PimGroup pimGroup = new PimGroup();
         pimGroupDAO.save(pimGroup);
         final PimUser pimUser = userWithMembershipAndPrimaryGroup(pimGroup, Role.ASSISTANT);
         pimUserDao.save(pimUser);
+
+        try (final Session session = SessionFactorySingleton.getSessionFactory().openSession()) {
+            final PimUser byUUID = pimUserDao.getByUUID(session, pimUser.getUuid());
+
+            assertThat(permissionHandler.isAllowedToUpdate(session, byUUID, SUBJECT_UUID), is(false));
+        }
+    }
+
+    @Test
+    public void isAllowedToUpdateReturnsFalseWhenTheUserIsDisabled() {
+        final PimGroup pimGroup = new PimGroup();
+        pimGroupDAO.save(pimGroup);
+        final PimUser pimUser = userWithMembershipAndPrimaryGroup(pimGroup, Role.RESEARCHER);
+        pimUser.setDisabled(true);
+        pimUserDao.save(pimUser);
+        final AclDao aclDao = this.aclDao;
+        final Acl acl = Acl.updatePermission(SUBJECT_UUID, pimGroup, Role.RESEARCHER);
+        aclDao.save(acl);
 
         try (final Session session = SessionFactorySingleton.getSessionFactory().openSession()) {
             final PimUser byUUID = pimUserDao.getByUUID(session, pimUser.getUuid());
@@ -327,6 +376,21 @@ public class PermissionHandlerTest {
         }
     }
 
+    @Test
+    public void isAllowedToUpdateReturnsFalseWhenTheUserIsDisabledWithoutUsingGroups() {
+        doNotUseGroups();
+        final PimUser pimUser = userWithRoles(List.of(Role.ADMIN));
+        pimUser.setDisabled(true);
+
+        pimUserDao.save(pimUser);
+
+        try (final Session session = SessionFactorySingleton.getSessionFactory().openSession()) {
+            final PimUser byUUID = pimUserDao.getByUUID(session, pimUser.getUuid());
+
+            assertThat(permissionHandler.isAllowedToUpdate(session, byUUID, SUBJECT_UUID), is(false));
+        }
+    }
+
 
     @Test
     public void isAllowedToDeleteReturnsTrueForAdmin() {
@@ -346,6 +410,37 @@ public class PermissionHandlerTest {
         pimGroupDAO.save(pimGroup);
         final PimUser pimUser = userWithMembershipAndPrimaryGroup(pimGroup, Role.ASSISTANT);
         pimUserDao.save(pimUser);
+
+        try (final Session session = SessionFactorySingleton.getSessionFactory().openSession()) {
+            final PimUser byUUID = pimUserDao.getByUUID(session, pimUser.getUuid());
+
+            assertThat(permissionHandler.isAllowedToDelete(session, byUUID, SUBJECT_UUID), is(false));
+        }
+    }
+
+    @Test
+    public void isAllowedToDeleteReturnsWhenTheAdminUserIsDisabled() {
+        final PimUser pimUser = adminUser();
+        pimUser.setDisabled(true);
+        pimUserDao.save(pimUser);
+
+        try (final Session session = SessionFactorySingleton.getSessionFactory().openSession()) {
+            final PimUser byUUID = pimUserDao.getByUUID(session, pimUser.getUuid());
+
+            assertThat(permissionHandler.isAllowedToDelete(session, byUUID, SUBJECT_UUID), is(false));
+        }
+    }
+
+    @Test
+    public void isAllowedToDeleteReturnsFalseWhenTheUserIsDisabled() {
+        final PimGroup pimGroup = new PimGroup();
+        pimGroupDAO.save(pimGroup);
+        final PimUser pimUser = userWithMembershipAndPrimaryGroup(pimGroup, Role.PI);
+        pimUser.setDisabled(true);
+        pimUserDao.save(pimUser);
+        final Acl acl = Acl.deletePermission(SUBJECT_UUID, pimGroup, Role.PI);
+        this.aclDao.save(acl);
+
 
         try (final Session session = SessionFactorySingleton.getSessionFactory().openSession()) {
             final PimUser byUUID = pimUserDao.getByUUID(session, pimUser.getUuid());
@@ -379,6 +474,21 @@ public class PermissionHandlerTest {
             final PimUser byUUID = pimUserDao.getByUUID(session, pimUser.getUuid());
 
             assertThat(permissionHandler.isAllowedToDelete(session, byUUID, SUBJECT_UUID), is(true));
+        }
+    }
+
+    @Test
+    public void isAllowedToDeleteReturnsFalseIfTheUserIsDisabledWithoutUsingGroups() {
+        doNotUseGroups();
+        final PimUser pimUser = userWithRoles(List.of(Role.PI));
+        pimUser.setDisabled(true);
+
+        pimUserDao.save(pimUser);
+
+        try (final Session session = SessionFactorySingleton.getSessionFactory().openSession()) {
+            final PimUser byUUID = pimUserDao.getByUUID(session, pimUser.getUuid());
+
+            assertThat(permissionHandler.isAllowedToDelete(session, byUUID, SUBJECT_UUID), is(false));
         }
     }
 
@@ -536,6 +646,30 @@ public class PermissionHandlerTest {
     }
 
     @Test
+    public void isAllowedToReadReturnsFalseIfTheUserIsDisabled() {
+        final PimGroup supergroup = new PimGroup();
+        pimGroupDAO.save(supergroup);
+        final PimGroup subgroup = new PimGroup();
+        subgroup.addSupergroup(supergroup);
+        pimGroupDAO.save(subgroup);
+        try(final Session session = SessionFactorySingleton.getSessionFactory().openSession()) {
+            final Transaction transaction = session.beginTransaction();
+            final Acl readPermission = Acl.readPermission(SUBJECT_UUID, pimGroupDAO.getByUUID(session, supergroup.getUuid()), Role.ASSISTANT);
+            this.aclDao.save(session, readPermission);
+            transaction.commit();
+        }
+        final PimUser pimUser = userWithMembershipAndPrimaryGroup(subgroup, Role.ASSISTANT);
+        pimUser.setDisabled(true);
+        pimUserDao.save(pimUser);
+
+        try (final Session session = SessionFactorySingleton.getSessionFactory().openSession()) {
+            final PimUser byUUID = pimUserDao.getByUUID(session, pimUser.getUuid());
+
+            assertThat(permissionHandler.isAllowedToRead(session, SUBJECT_UUID, byUUID), is(false));
+        }
+    }
+
+    @Test
     public void isAllowedToReadReturnsTrueForAdmins() {
         final PimUser admin = userWithRoles(List.of(Role.ADMIN));
         pimUserDao.save(admin);
@@ -556,6 +690,19 @@ public class PermissionHandlerTest {
         try (final Session session = SessionFactorySingleton.getSessionFactory().openSession()) {
 
             assertThat(permissionHandler.isAllowedToRead(session, SUBJECT_UUID, pimUser), is(true));
+        }
+    }
+
+    @Test
+    public void isAllowedToReadReturnsFalseWhenNoGroupsAreUsedAndTheUserIsDisabled() {
+        doNotUseGroups();
+        final PimUser pimUser = userWithRoles(List.of(Role.AUTHENTICATED));
+        pimUser.setDisabled(true);
+        pimUserDao.save(pimUser);
+
+        try (final Session session = SessionFactorySingleton.getSessionFactory().openSession()) {
+
+            assertThat(permissionHandler.isAllowedToRead(session, SUBJECT_UUID, pimUser), is(false));
         }
     }
 
