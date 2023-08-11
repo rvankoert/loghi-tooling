550c0f3c346ead679d0f165c6699c25e21dfd177
Martijn Maas <martijn.maas@di.huc.knaw.nl>
2023-08-11 15:25:35 +0200

    Fix getFileName of DocumentImage

--- layoutds/src/test/java/DocumentImageTest.java
+++ layoutds/src/test/java/DocumentImageTest.java
@@ -13,6 +13,8 @@ import java.lang.reflect.Field;
 import java.util.List;
 import java.util.UUID;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {StudentJpaConfig.class}, loader = AnnotationConfigContextLoader.class)
 public class DocumentImageTest {
@@ -31,7 +33,7 @@ public class DocumentImageTest {
         documentImageDAO.save(documentImage);
 
         results = documentImageDAO.getAll();
-        Assert.assertEquals(count + 2, results.size());
+        assertEquals(count + 2, results.size());
     }
 
     @Test
@@ -49,6 +51,39 @@ public class DocumentImageTest {
                 System.out.println(field.getType().toString());
             }
         }
+    }
+
+    @Test
+    public void getFileNameReturnsTheLastPartOfTheUri() {
+        final DocumentImage documentImage = new DocumentImage();
+        documentImage.setUri("/data/images/1.11.06.11_Cochin/1257B/NL-HaNA_1.11.06.11_1257B_0001.jpg");
+
+        assertEquals("NL-HaNA_1.11.06.11_1257B_0001.jpg", documentImage.getFileName());
+    }
+
+    @Test
+    public void getFileNameStripsTheIIIFPathToFindTheFileName() {
+        final DocumentImage documentImage = new DocumentImage();
+        documentImage.setUri("https://www.e-codices.unifr.ch/loris/kba/kba-MurQ0007/kba-MurQ0007_0000_0001v.jp2/full/full/0/default/jpg");
+
+        assertEquals("kba-MurQ0007_0000_0001v.jp2", documentImage.getFileName());
+    }
+
+    @Test
+    public void getFileNameStripsIIFPathToFindTheFileNameEvenWhenItEndsOnAFileName() {
+        final DocumentImage documentImage = new DocumentImage();
+        documentImage.setUri("http://www.e-codices.unifr.ch:80/loris/ubb/ubb-A-II-0029/ubb-A-II-0029_0012r.jp2/full/full/0/default.jpg");
+
+        assertEquals("ubb-A-II-0029_0012r.jp2", documentImage.getFileName());
+    }
+
+
+    @Test
+    public void getFileNameStripsTheIIIFPathToFindTheFileNameEvenWhenImageNameIsHidden() {
+        final DocumentImage documentImage = new DocumentImage();
+        documentImage.setUri("https://stacks.stanford.edu/image/iiif/fs743fm9703/046_145_R_TC_46/full/full/0/default.jpg");
+
+        assertEquals("046_145_R_TC_46.jpg", documentImage.getFileName());
     }
 
 }
