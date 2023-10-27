d7d51b8f6b7e98e2add40c5d61e8fac92eee2aa3
Rutger <r.c.van.koert@gmail.com>
2023-10-27 16:46:57 +0200

    cleanup

--- layoutds/src/main/java/nl/knaw/huc/di/images/layoutds/models/Page/Coords.java
+++ layoutds/src/main/java/nl/knaw/huc/di/images/layoutds/models/Page/Coords.java
@@ -9,7 +9,7 @@ import java.util.regex.Pattern;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class Coords {
-    private static Pattern pointsPattern = Pattern.compile("([0-9]+,[0-9]+ )+([0-9]+,[0-9]+)");
+    private static final Pattern pointsPattern = Pattern.compile("([0-9]+,[0-9]+ )+([0-9]+,[0-9]+)");
 
     @JacksonXmlProperty(isAttribute = true, localName = "points")
     private String points;
@@ -20,7 +20,6 @@ public class Coords {
 
     public void setPoints(String points) {
         // regex to check if points is a valid string
-        // https://stackoverflow.com/questions/163360/regular-expression-to-match-urls-in-java
         Matcher matcher = pointsPattern.matcher(points);
         if (!matcher.matches()){
             throw new IllegalArgumentException("Points string is not valid");
--- layoutds/src/test/java/nl/knaw/huc/di/images/layoutds/CoordsTest.java
+++ layoutds/src/test/java/nl/knaw/huc/di/images/layoutds/CoordsTest.java
@@ -1,32 +1,7 @@
 package nl.knaw.huc.di.images.layoutds;
 
-import nl.knaw.huc.di.images.layoutds.DAO.DocumentImageDAO;
-import nl.knaw.huc.di.images.layoutds.DAO.DocumentImageSetDAO;
-import nl.knaw.huc.di.images.layoutds.models.DocumentImage;
-import nl.knaw.huc.di.images.layoutds.models.DocumentImageSet;
-import nl.knaw.huc.di.images.layoutds.models.MetaData;
 import nl.knaw.huc.di.images.layoutds.models.Page.Coords;
-import org.apache.commons.lang3.tuple.Pair;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
-
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.hasProperty;
 
 public class CoordsTest {
     @Test(expected = IllegalArgumentException.class)
