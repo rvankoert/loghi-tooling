0f6af2f314de3eaaa6143da1c1c5119200792404
Rutger van Koert <r.c.van.koert@gmail.com>
2025-07-16 15:28:35 +0200

    add IIIFTypes

--- layoutds/src/main/java/nl/knaw/huc/di/images/layoutds/models/iiif/IIIFTypes.java
+++ layoutds/src/main/java/nl/knaw/huc/di/images/layoutds/models/iiif/IIIFTypes.java
@@ -26,13 +26,30 @@ public enum IIIFTypes {
     Image("dctypes:Image"),
     AnnotationPage("sc:AnnotationPage"),
 //    Service("dctypes:Service"),
-    ImageService2("dctypes:ImageService2"),;
+    ImageService2("dctypes:ImageService2"),
+    Dataset("Dataset"),;
 
 
     private final String text;
 
     IIIFTypes(final String text) {
         this.text = text;
+    }
+
+    public static IIIFTypes parseType(String string) {
+        for (IIIFTypes type : IIIFTypes.values()) {
+            if (type.text.equalsIgnoreCase(string)) {
+                return type;
+            }
+            String[] splitted = type.toString().split(":");
+            if (splitted.length > 1 && splitted[1].equalsIgnoreCase(string)) {
+                return type;
+            }
+        }
+        if ("manifest".equalsIgnoreCase(string)) {
+            return IIIFTypes.Manifest;
+        }
+        return null;
     }
 
     @Override
